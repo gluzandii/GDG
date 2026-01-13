33e41e964425461b0bb0a8481de18585970e2bb9
Sushant Nariangadu <nsushant2007@gmail.com>
2026-01-13 16:51:30 +0530

    Setup postgres.
    The postgres database has been setup along with migration setup etc.

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,24 +6,7 @@ use axum::{routing::get, Router};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use std::env;
-use std::path::Path;
 
-#[cfg(debug_assertions)]
-#[inline(always)]
-fn load_env() {
-    match dotenvy::dotenv() {
-        Ok(path) => println!("Loaded .env via dotenv(): {}", path.display()),
-        Err(_) => {
-            // fallback: load `.env` from the crate (server) directory
-            let manifest = env!("CARGO_MANIFEST_DIR"); // points to `server` at compile time
-            let env_path = Path::new(manifest).join(".env");
-            match dotenvy::from_path(&env_path) {
-                Ok(_) => println!("Loaded .env from {}", env_path.display()),
-                Err(e) => println!("Failed to load {}: {}", env_path.display(), e),
-            }
-        }
-    }
-}
 #[inline(always)]
 async fn setup_db() -> Pool<Postgres> {
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
@@ -41,7 +24,7 @@ async fn setup_db() -> Pool<Postgres> {
 #[tokio::main]
 async fn main() {
     #[cfg(debug_assertions)]
-    load_env();
+    dotenvy::dotenv().ok();
 
     let port = env::var("PORT").unwrap_or_else(|_| "2607".into());
     let addr = format!("127.0.0.1:{}", port);
