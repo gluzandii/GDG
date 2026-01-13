57c4aa826b07a925f6509ee6ae0733ca2ab63542
Sushant Nariangadu <nsushant2007@gmail.com>
2026-01-13 16:27:58 +0530

    Setup ORM (sqlx)
    Using rust sqlx for ORM along with Postgres as the entire backend for this chat application.
    
    setup the database pool too.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,15 @@
 mod routes;
 
+use crate::routes::auth::register::api::register;
 use axum::routing::post;
 use axum::{routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
 use std::env;
 use std::path::Path;
 
-use crate::routes::auth::register::api::register;
-
 #[cfg(debug_assertions)]
+#[inline(always)]
 fn load_env() {
     match dotenvy::dotenv() {
         Ok(path) => println!("Loaded .env via dotenv(): {}", path.display()),
@@ -22,6 +24,19 @@ fn load_env() {
         }
     }
 }
+#[inline(always)]
+async fn setup_db() -> Pool<Postgres> {
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(std::time::Duration::from_secs(5))
+        .connect(&db_url)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    pool
+}
 
 #[tokio::main]
 async fn main() {
@@ -31,14 +46,16 @@ async fn main() {
     let port = env::var("PORT").unwrap_or_else(|_| "2607".into());
     let addr = format!("127.0.0.1:{}", port);
 
+    let pool = setup_db().await;
+
     let app = Router::new()
         .route("/health", get(health))
-        .route("/auth/register", post(register));
+        .route("/auth/register", post(register))
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
 
     println!("Listening on http://{}", addr);
-
     axum::serve(listener, app).await.unwrap();
 }
 
